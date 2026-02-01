5a9e7595561d9e4bd9b60a8f4162e512dac75fd6
Kesiena Berezi <98287602+kb11031207@users.noreply.github.com>
2026-02-01 15:06:44 -0600

    Revert "Revert "Updated team members details and date off the events ""

--- lib/team-members.ts
+++ lib/team-members.ts
@@ -18,7 +18,7 @@ export const teamMembers: TeamMember[] = [
     class: "Senior",
     majors: "Global Studies and Sociology & Anthropology",
     hometown: "Xicotepec de Juarez, Puebla Mexico",
-    image: "https://prinweb.principia.edu/phone/new_faces_pictures/Andover_Extract_Files/32804279.jpg",
+    image: "https://github.com/chrisrama/pac-website-design/blob/main/public/Saray-headshot.jpeg?raw=truepublic/Saray-headshot.jpeg",
     bio: "Social change starts when we take the lead and start realizing there is more to life beyond ourselves. As a Mexican, the ethnicity with most immigrants in the United States, I felt there was a need for me to step outside my privilege and help educate people on immigration issues that my community is being affected on. There is a need for our society to realize that we live in an interconnected world. Being a global citizen involves empathizing, educating yourself and others, and raising awareness about the impact you make at a global scale. That's why I chose to direct PAC",
   },
   {
@@ -29,7 +29,7 @@ export const teamMembers: TeamMember[] = [
     majors: "Global Studies and Educational Studies",
     minors: "Sustainability, Sociology & Anthropology, and Religious Studies",
     hometown: "Lüneburg, Germany",
-    image: "https://prinweb.principia.edu/phone/new_faces_pictures/Andover_Extract_Files/86598043.jpg",
+    image: "https://github.com/chrisrama/pac-website-design/blob/ea27775cd54dbff67d220a4f6ea962dcb4de61ae/public/Maria-headshot.jpeg?raw=true",
     bio: "Education and knowledge are key drivers of inclusion, acceptance, and critical thinking. Being a part of PAC allows me to be a part of building a community that provides room for this and to help people understand and see experiences other than their own.",
   },
   {
@@ -40,7 +40,7 @@ export const teamMembers: TeamMember[] = [
     majors: "Global Studies, Sustainability",
     minors: "Ecology, Sociology & Anthropology",
     hometown: "Grecia, Alajuela, Costa Rica",
-    image: "https://prinweb.principia.edu/phone/new_faces_pictures/Andover_Extract_Files/66031342.jpg",
+    image: "https://github.com/chrisrama/pac-website-design/blob/ea27775cd54dbff67d220a4f6ea962dcb4de61ae/public/Julian-headshot.jpeg?raw=true",
     bio: '"We choose to go to the moon and do the other things not because they are easy but because they are hard." This quote is from a speech given by President John F. Kennedy at Rice University (1962), and it is his famous challenge to the nation to achieve the goal of landing a man on the moon. I choose to be on PAC this semester to again challenge the nation - to challenge our beliefs and to grow past our biases and comfort; a challenge not to be on a particular side, but to come together as one nation comprised of many unique individuals.',
   },
   {
@@ -60,13 +60,13 @@ export const teamMembers: TeamMember[] = [
     class: "Senior",
     majors: "Finance and Economics",
     hometown: "Balcarce, Buenos Aires, Argentina",
-    image: "https://prinweb.principia.edu/phone/new_faces_pictures/Andover_Extract_Files/31415511.jpg",
+    image: "https://github.com/chrisrama/pac-website-design/blob/ea27775cd54dbff67d220a4f6ea962dcb4de61ae/public/Francisco-headshot.jpeg?raw=true",
     bio: "I am a part of PAC because immigration is personal to me, it's my story. I grew up in Balcarce, Argentina, and at thirteen I decided to leave home and pursue a new life in the United States. After years of challenges and persistence, I earned a scholarship to Principia, and that experience taught me that we are not just citizens of one country but of a larger, connected world. I care about this conference because immigration is not just policy, it's indeed people, sacrifice, and hope. Through my journey and what I've learned at Principia, I've seen how diverse stories create understanding and community.This conference will show that empathy and connection can be strengthened with a new approach, one that listens to and values immigrant voices. That's why I'm part of PAC.",
   },
   {
     name: "Ronnie Ochieng",
     slug: "ronnie-ochieng",
-    title: "Co-director",
+    title: "Excutive Director ",
     class: "Sophomore",
     majors: "Global Studies and Business",
     hometown: "Migori, Kenya",
@@ -76,11 +76,11 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Christian Ramazani",
     slug: "christian-ramazani",
-    title: "Co-director",
+    title: "Co-director Publication",
     class: "Sophomore",
     majors: "Computer Science and Economics",
     hometown: "Cape Town, South Africa",
-    image: "https://prinweb.principia.edu/phone/new_faces_pictures/Andover_Extract_Files/86756220.jpg",
+    image: "https://github.com/chrisrama/pac-website-design/blob/main/public/Christian-headshot.jpg?raw=true",
     bio: "I am part of PAC because I help to share the message of values immigrants have created in order for people to understand better why immigration is a good thing.",
   },
   {
@@ -88,10 +88,21 @@ export const teamMembers: TeamMember[] = [
     slug: "agatha-nakalembe",
     title: "Co-Director of Publications",
     class: "Freshman",
-    majors: "Business & Global Studies",
+    majors: "Business, Mathematics & Global Studies",
     hometown: "Kampala, Uganda",
-    image: "https://prinweb.principia.edu/phone/new_faces_pictures/Andover_Extract_Files/98558631.jpg",
+    image: "https://github.com/chrisrama/pac-website-design/blob/ea27775cd54dbff67d220a4f6ea962dcb4de61ae/public/Agatha-headshot.jpeg?raw=true",
     bio: "I'm part of PAC because I love what it stands for/objective and I want to be part of something that positively makes an impact on people.",
+  },
+  {
+    name: "Ernesto Botero",
+    slug: "ernesto-botero",
+    title: "Off-Campus Delegate",
+    class: "Senior",
+    majors: "Sociology & Anthropology",
+    hometown: "Cartagena, Colombia",
+    image: "https://github.com/chrisrama/pac-website-design/blob/ea27775cd54dbff67d220a4f6ea962dcb4de61ae/public/Ernesto-headshot.jpeg?raw=true",
+    bio: "I am part of PAC because I want to keep this Principia tradition running and also because I want to help raise awareness in the Principia community about contemporary issues.",
+
   },
 ]
 
